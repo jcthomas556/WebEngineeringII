f766d9c46532438e9db06443fad8bf9d382ebf18
Jacob <jcthomas556@byui.com>
2019-09-26 19:14:41 -0600

    note

--- web/Week2/AboutMe.php
+++ web/Week2/AboutMe.php
@@ -19,7 +19,7 @@
             for the cooler weather! <p>
     </div>
 
-    <img src="family.jpg" alt="Townsend's Warbler"  class="picture">
+    <img src="family.jpg" alt="family photo"  class="center">
 
 
     <footer>
