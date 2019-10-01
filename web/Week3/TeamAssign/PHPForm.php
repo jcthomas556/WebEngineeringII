96b834cdef8de9df2190322f086ef0c94b32bb3d
Jacob <jcthomas556@byui.com>
2019-10-01 15:20:46 -0600

    test3

--- web/Week3/TeamAssign/PHPForm.php
+++ web/Week3/TeamAssign/PHPForm.php
@@ -11,12 +11,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     echo "User Name: $name <br> User Email Address: <a href='mailto:$email'>$email</a> 
-    <br> User Major: $major <br> User Comments: $comments";
+    <br> User Major: $major <br> User Comments: $comments <br> User has visited these continents: ";
+
+
 
     if(!empty($continents)){
         for($i=0; $i < count($continents); $i++)
         {
-            echo($continents[$i] . " ");
+            if($i > 0){
+                echo ", ";
+            }
+            echo($continents[$i]);
         }
     }
 
