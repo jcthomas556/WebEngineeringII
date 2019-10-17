7b3acee57b35449e093678b9427749f576153980
Jacob <jcthomas556@byui.com>
2019-10-17 15:36:55 -0600

    intitiative of the Initiative Tracker

---
+++ web/Week5/InitTracLanding.php
@@ -0,0 +1,40 @@
+<?php
+
+try {
+    $dbvars = parse_url(getenv('DATABASE_URL'));
+​
+    $dbHost = $dbvars['host'];
+    $dbPort = $dbvars['port'];
+    $dbUser = $dbvars['user'];
+    $dbPass = $dbvars['pass'];
+    $dbName = ltrim($dbvars['path'], '/');
+​
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+​
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $ex->getMessage();
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Activity</title>
+</head>
+<body>
+    <h1>Initiative Tracker</h1>
+    <h3>All player characters</h3>
+    <?php
+    foreach ($db->query('SELECT * FROM player_characters', PDO::FETCH_ASSOC) as $row)
+    {
+        echo '<p><b>' . $row['player_fname'] . ' ' . $row['player_lname'] . 'AC:' . $row['player_ac'] . '</b> -- "' . $row['player_race'] . ', "' . $row['player_class']  '"</p>';
+    }
+    ?>
+    <br><br>
+
+</body>
+</html>
---
+++ web/Week5/Inserts.sql
@@ -0,0 +1,81 @@
+INSERT into npc_characters (npc_fname, npc_lname, npc_ac, npc_init_bonus, npc_race_type, enemy) 
+VALUES (
+    'Jim',
+    'Jammin',
+    '16',
+    '3',
+    'humanoid',
+    'f'
+    );
+
+
+INSERT into npc_characters (npc_fname, npc_lname, npc_ac, npc_init_bonus, npc_race_type, enemy) 
+VALUES (
+    'Bob',
+    'Belcher',
+    '17',
+    '-2',
+    'humanoid',
+    'f'
+    );
+
+
+INSERT into npc_characters (npc_fname, npc_lname, npc_ac, npc_init_bonus, npc_race_type, enemy) 
+VALUES (
+    'Louise',
+    'Belcher',
+    '10',
+    '7',
+    'child',
+    't'
+    );
+
+INSERT into player_characters (player_fname, player_lname, player_ac, player_init_bonus, player_race, player_class) 
+VALUES (
+    'Saul',
+    'Solarian',
+    '14',
+    '1',
+    'Aasimar',
+    'Sorcerer'
+    );
+
+INSERT into player_characters (player_fname, player_lname, player_ac, player_init_bonus, player_race, player_class) 
+VALUES (
+    'Flouise',
+    'Knight',
+    '18',
+    '-1',
+    'Water Genasi',
+    'Paladin'
+    );
+
+INSERT into player_characters (player_fname, player_lname, player_ac, player_init_bonus, player_race, player_class) 
+VALUES (
+    'Arra',
+    'Khai',
+    '14',
+    '3',
+    'Aarakocra',
+    'Ranger'
+    );
+
+
+INSERT into images (player_character, img_description, img) 
+VALUES (
+    't',
+    'saul_image',
+    bytea('saul_image.jpg')
+    );
+
+
+UPDATE player_characters
+SET image_id = 
+    (
+        SELECT image_id
+        FROM images
+        WHERE img_description = 'saul_image'
+    ) 
+WHERE
+   player_fname = 'Saul';
+
