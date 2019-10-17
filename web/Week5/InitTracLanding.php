6b4335cdacb6650d8b661536b66b959f0e3a76a0
Jacob <jcthomas556@byui.com>
2019-10-17 16:05:46 -0600

    test7

--- web/Week5/InitTracLanding.php
+++ web/Week5/InitTracLanding.php
@@ -1,22 +1,6 @@
 <?php
-​
-try {
-    $dbvars = parse_url(getenv('DATABASE_URL'));
-​
-    $dbHost = $dbvars['host'];
-    $dbPort = $dbvars['port'];
-    $dbUser = $dbvars['user'];
-    $dbPass = $dbvars['pass'];
-    $dbName = ltrim($dbvars['path'], '/');
-​
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-​
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $ex->getMessage();
-    die();
-}
-​
+    require "../databaseConnection.php";
+    $db = get_db();
 ?>
 
 <!DOCTYPE html>
