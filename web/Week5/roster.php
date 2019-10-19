b65bb70a55bc3581bc490a1a09dbf22f3a5e655d
Jacob <jcthomas556@byui.com>
2019-10-18 19:18:02 -0600

    phpAdditions11

--- web/Week5/roster.php
+++ web/Week5/roster.php
@@ -39,7 +39,7 @@
         
                     foreach ($db->query("SELECT * FROM player_characters WHERE player_fname='$name'", PDO::FETCH_ASSOC) as $row)
                     {
-                   //     echo '<p>' . $row['fname'] . ' ' . $row['lname'] . '</p>';
+                        echo '<p>' . $row['fname'] . ' ' . $row['lname'] . '</p>';
                         //echo '<p><a href="scripture.php?id=' . $row['id'] . '"><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b></a></p>';
                     }
                 }
