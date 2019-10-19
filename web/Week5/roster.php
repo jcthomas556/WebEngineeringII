8cc11104971f43061b4c9c3310dd6ae11d65ef1d
Jacob <jcthomas556@byui.com>
2019-10-18 20:10:06 -0600

    phpModifications2

--- web/Week5/roster.php
+++ web/Week5/roster.php
@@ -39,12 +39,21 @@
         
                     foreach ($db->query("SELECT * FROM player_characters WHERE player_fname='$name'", PDO::FETCH_ASSOC) as $row)
                     {
-                        echo '<p>' . $row['player_fname'] . ' ' . $row['player_lname'] . '</p>';
+                        echo '<p>' . $row['player_fname'] . ' ' . $row['player_lname'] . 'The '. $row['player_race'] . ', '. $row['player_class'] . 'AC of ' . $row['player_ac'] . 'initiative of ' . $row['player_init_bonus'] .  '</p>';
                     }
                     foreach ($db->query("SELECT * FROM npc_characters WHERE npc_fname = '$name'", PDO::FETCH_ASSOC) as $row)
                     {
-                        echo '<p>' . $row['npc_fname'] . ' ' . $row['npc_lname'] . '</p>';
+                        echo '<p>' . $row['npc_fname'] . ' ' . $row['npc_lname'] . 'The '. $row['npc_race_type'] . 'AC of ' . $row['player_ac'] . 'initiative of ' . $row['player_init_bonus'] . '</p>';
                     }
+                    foreach ($db->query("SELECT * FROM player_characters WHERE player_lname='$name'", PDO::FETCH_ASSOC) as $row)
+                    {
+                        echo '<p>' . $row['player_fname'] . ' ' . $row['player_lname']  .  'The '. $row['player_race'] . ', '. $row['player_class'] . 'AC of ' . $row['player_ac'] . 'initiative of ' . $row['player_init_bonus'] . '</p>';
+                    }
+                    foreach ($db->query("SELECT * FROM npc_characters WHERE npc_lname = '$name'", PDO::FETCH_ASSOC) as $row)
+                    {
+                        echo '<p>' . $row['npc_fname'] . ' ' . $row['npc_lname'] . $row['npc_race_type'] . 'AC of ' . $row['player_ac'] . 'initiative of ' . $row['player_init_bonus'] . '</p>';
+                    }
+                    //if search is blank, return all from everything!
                 }
             ?>
             
