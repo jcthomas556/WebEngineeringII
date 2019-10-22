214b848ae77bd4af134216a4d0194270b77e0079
Jacob <jcthomas556@byui.com>
2019-10-22 15:23:24 -0600

    teamAssign6.3

--- web/Week5/TeamAssign/scripture.php
+++ web/Week5/TeamAssign/scripture.php
@@ -1,19 +1,29 @@
 <?php
-try {
-    $dbvars = parse_url(getenv('DATABASE_URL'));
-​
-    $dbHost = $dbvars['host'];
-    $dbPort = $dbvars['port'];
-    $dbUser = $dbvars['user'];
-    $dbPass = $dbvars['pass'];
-    $dbName = ltrim($dbvars['path'], '/');
-​
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-​
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $ex->getMessage();
-    die();
+function get_db() {
+    $db = NULL;
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    
+    return $db;
 }
 ?>
 <!DOCTYPE html>
