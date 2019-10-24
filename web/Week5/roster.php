6595e607d0110ef60aa7ddf2cc202f7bd6a3736b
Jacob <jcthomas556@byui.com>
2019-10-24 15:53:37 -0600

    vardump3

--- web/Week5/roster.php
+++ web/Week5/roster.php
@@ -36,24 +36,24 @@
 
            
             <?php
-                foreach($db->query(
-                "SELECT
-                    player_fname,
-                    player_lname,
-                    date_entered
-                 FROM
-                    player_characters
-                 UNION
-                 SELECT
-                    npc_fname,
-                    npc_lname,
-                    date_entered
-                 FROM
-                    npc_characters
-                 ORDER BY date_entered LIMIT 5", PDO::FETCH_ASSOC) as $holder)
-                 {
-                    echo '<p>' . $holder['player_lname'] . ', ' . $holder['player_fname'] . '</p>';
-                 }
+                // foreach($db->query(
+                // "SELECT
+                //     player_fname,
+                //     player_lname,
+                //     date_entered
+                //  FROM
+                //     player_characters
+                //  UNION
+                //  SELECT
+                //     npc_fname,
+                //     npc_lname,
+                //     date_entered
+                //  FROM
+                //     npc_characters
+                //  ORDER BY date_entered LIMIT 5", PDO::FETCH_ASSOC) as $holder)
+                //  {
+                //     echo '<p>' . $holder['player_lname'] . ', ' . $holder['player_fname'] . '</p>';
+                //  }
 
 
 
