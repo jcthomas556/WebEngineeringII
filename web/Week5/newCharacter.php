d82a11cec868d7ff218e79e056303e8ad1eafca2
Jacob <jcthomas556@byui.com>
2019-10-24 20:13:35 -0600

    unique+upper10

--- web/Week5/newCharacter.php
+++ web/Week5/newCharacter.php
@@ -17,6 +17,18 @@
 
   
 
+  <script type="text/javascript">
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+    $('.fname').keyup(function(event) {
+        var textBox = event.target;
+        var start = textBox.selectionStart;
+        var end = textBox.selectionEnd;
+        textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1).toLowerCase();
+        textBox.setSelectionRange(start, end);
+    });
+});
+</script>
 
 
 </head>
@@ -97,18 +109,6 @@
   
 </div>
 
-<script type="text/javascript">
-jQuery.noConflict();
-jQuery(document).ready(function($) {
-    $('.fname').keyup(function(event) {
-        var textBox = event.target;
-        var start = textBox.selectionStart;
-        var end = textBox.selectionEnd;
-        textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1).toLowerCase();
-        textBox.setSelectionRange(start, end);
-    });
-});
-</script>
 
 
 
