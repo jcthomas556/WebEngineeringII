c63d894ce2b2338fa36461e1e33b3581f656a895
Jacob <jcthomas556@byui.com>
2019-10-30 21:41:42 -0600

    javascript32

--- web/Week5/roster.php
+++ web/Week5/roster.php
@@ -148,7 +148,7 @@ var list = document.getElementsByClassName("lists");
 var players = [""];
 for (let i = 0; i < list.length; i++) {
     list[i].onclick = function() { list[i].style.color = "green"; }
-    list[i].onclick = addIt() { players[i] = list[i]; }
+    //list[i].onclick = addIt() { players[i] = list[i]; }
     
 
 }
