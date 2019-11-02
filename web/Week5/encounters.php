4fe0ae05eccfb84945576be5cf87ad404636ce9f
Jacob <jcthomas556@byui.com>
2019-11-02 00:05:31 -0600

    encounter129

--- web/Week5/encounters.php
+++ web/Week5/encounters.php
@@ -51,6 +51,45 @@
  </div>
 
 
+<script>
+var playersFight=[];
+document.addEventListener("DOMContentLoaded" , ()=>{
+    var result = JSON.parse(localStorage.getItem("storedPlayers"));
+    
+    for (var y = 0; y < result.length; y++){
+        playersFight.push(result[y])
+    }
+    console.log(playersFight);
+    // for (var y = 0; y < playersFight.length; y++){
+    //     players.push(playersFight[y])
+    // }
+
+    addPlayersEncounter();
+
+})
+  
+  var temp = 10;
+  function addPlayersEncounter(){
+    console.log(playersFight);
+    //make a new display array, push each element into it, then display i and i+1 
+
+    var displayArray = [];
+    for (i = 0; i < playersFight.length; i++){
+//uncomment everything and this should be ready to display in order. Recomment the current display tool
+        //displayArray.push(playersFight[i]);
+        //displayArray.push(diceRolls[i]);     
+    
+        document.getElementById("activePlayersEncounter").innerHTML += "<p class = 'lists' id = 'defaultList'> <span class='badge'> " + temp + "</span>" + playersFight[i] + "</p>";   
+        //temp = diceRolls[i];
+        }
+    // for (t = 0; t < displayArray.length; t = t+2){
+    //   document.getElementById("activePlayersEncounter").innerHTML += "<p class = 'lists' id = 'defaultList'> <span class='badge'> " + displayArray[t+1] + "</span>" + displayArray[t] + "</p>";   
+
+    // }
+ 
+
+    }
+</script>
 
 
 <?php
@@ -110,46 +149,6 @@
     //pass the global variables to the encounter page to be
 
  ?>
- <script>
-var playersFight=[];
-document.addEventListener("DOMContentLoaded" , ()=>{
-    var result = JSON.parse(localStorage.getItem("storedPlayers"));
-    
-    for (var y = 0; y < result.length; y++){
-        playersFight.push(result[y])
-    }
-    console.log(playersFight);
-    // for (var y = 0; y < playersFight.length; y++){
-    //     players.push(playersFight[y])
-    // }
-
-    addPlayersEncounter();
-
-})
-  
-  var temp = 10;
-  function addPlayersEncounter(){
-    console.log(playersFight);
-    //make a new display array, push each element into it, then display i and i+1 
-
-    var displayArray = [];
-    for (i = 0; i < playersFight.length; i++){
-//uncomment everything and this should be ready to display in order. Recomment the current display tool
-        //displayArray.push(playersFight[i]);
-        //displayArray.push(diceRolls[i]);     
-    
-        document.getElementById("activePlayersEncounter").innerHTML += "<p class = 'lists' id = 'defaultList'> <span class='badge'> " + temp + "</span>" + playersFight[i] + "</p>";   
-        //temp = diceRolls[i];
-        }
-    // for (t = 0; t < displayArray.length; t = t+2){
-    //   document.getElementById("activePlayersEncounter").innerHTML += "<p class = 'lists' id = 'defaultList'> <span class='badge'> " + displayArray[t+1] + "</span>" + displayArray[t] + "</p>";   
-
-    // }
- 
-
-    }
-</script>
-
 
 </body>
 </html>
