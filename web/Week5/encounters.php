00e9c89217b4aac746c5c58084e6597fc713f4ed
Jacob <jcthomas556@byui.com>
2019-11-01 21:33:45 -0600

    encounter100

--- web/Week5/encounters.php
+++ web/Week5/encounters.php
@@ -97,7 +97,7 @@ document.addEventListener("DOMContentLoaded" , ()=>{
       $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 
                 "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  
                 $_SERVER['REQUEST_URI']; 
-       
+       echo $link;
        $people = (explode(",", $link));
        //var_dump($people);
        //var_dump($group);
