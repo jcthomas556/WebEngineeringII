dd880a23d3602ab2da29ed2de9a4f5dd45729b83
Jacob <jcthomas556@byui.com>
2019-11-01 23:19:15 -0600

    encounter113

--- web/Week5/encounters.php
+++ web/Week5/encounters.php
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded" , ()=>{
                
               //  </script> </p>';
               echo '<script> document.getElementById("activePlayersEncounter").innerHTML += 
-              "<p> playersFight[1] </p>" </script>';
+              "<p> 'playersFight[1]' </p>" </script>';
 
               // echo '<script>  document.getElementById("activePlayersEncounter").innerHTML 
               // += "<p class = 'lists' id = 'defaultList' style='order = ' + temp + ' '> <span class='badge'> " 
